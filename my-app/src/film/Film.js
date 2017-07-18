9c0ea60c74b1e1271c3af77aa27e9d78c6ce86d1
andriyspas <151096as>
2017-07-18 16:18:51 +0200

    test http request

--- my-app/src/film/Film.js
+++ my-app/src/film/Film.js
@@ -1,6 +1,26 @@
-/**
- * Created by Stas on 18-Jul-17.
- */
 import React, { Component } from 'react';
 
-class Film extends Component {}
+export class Film extends Component {
+    test = () => {
+        fetch('http://localhost:8080/api/film/get/film/title/red', { method: 'GET' })
+            .then(resp => resp.json())
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+
+    constructor(properties) {
+        super(properties);
+
+        this.test();
+    }
+}
